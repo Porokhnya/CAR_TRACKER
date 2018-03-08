e76863efadcb904a01569ecc87db79c39bbe2dc5
Porokhnya Dmitry <spywarrior@gmail.com>
2018-03-08 20:40:51 +0300

    Подключил библиотеки

---
+++ MAIN/DS3231.cpp
@@ -0,0 +1,310 @@
+#include "DS3231.h"
+//--------------------------------------------------------------------------------------------------------------------------------------
+char DS3231Clock::workBuff[12] = {0};
+//--------------------------------------------------------------------------------------------------------------------------------------
+DS3231Clock RTCClock;
+//--------------------------------------------------------------------------------------------------------------------------------------
+const uint8_t daysArray [] PROGMEM = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+//--------------------------------------------------------------------------------------------------------------------------------------
+uint32_t DS3231Time::unixtime(void)
+{
+  uint32_t u;
+
+  u = time2long(date2days(this->year, this->month, this->dayOfMonth), this->hour, this->minute, this->second);
+  u += 946684800; // + 01.01.2000 00:00:00 unixtime
+
+  return u;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+DS3231Time DS3231Time::addDays(long days)
+{
+  uint32_t ut = unixtime();
+  long diff = days*86400;
+  ut += diff;  
+  return fromUnixtime(ut);
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+DS3231Time DS3231Time::fromUnixtime(uint32_t time)
+{
+  DS3231Time result;
+
+  uint8_t year;
+  uint8_t month, monthLength;
+  unsigned long days;
+
+  result.second = time % 60;
+  time /= 60; // now it is minutes
+  result.minute = time % 60;
+  time /= 60; // now it is hours
+  result.hour = time % 24;
+  time /= 24; // now it is days
+  
+  year = 0;  
+  days = 0;
+  while((unsigned)(days += (isLeapYear(year) ? 366 : 365)) <= time) {
+    year++;
+  }
+  result.year = year + 1970; // year is offset from 1970 
+  
+  days -= isLeapYear(year) ? 366 : 365;
+  time  -= days; // now it is days in this year, starting at 0
+  
+  days=0;
+  month=0;
+  monthLength=0;
+  for (month=0; month<12; month++) 
+  {
+    if (month==1) 
+    { // february
+      if (isLeapYear(year)) 
+      {
+        monthLength=29;
+      } 
+      else 
+      {
+        monthLength=28;
+      }
+    } 
+    else 
+    {
+      monthLength = pgm_read_byte(daysArray + month); //monthDays[month];
+    }
+    
+    if (time >= monthLength) 
+    {
+      time -= monthLength;
+    } 
+    else 
+    {
+        break;
+    }
+  }
+  result.month = month + 1;  // jan is month 1  
+  result.dayOfMonth = time + 1;     // day of month  
+
+    int dow;
+    byte mArr[12] = {6,2,2,5,0,3,5,1,4,6,2,4};
+    dow = (result.year % 100);
+    dow = dow*1.25;
+    dow += result.dayOfMonth;
+    dow += mArr[result.month-1];
+    
+    if (isLeapYear(result.year) && (result.month<3))
+     dow -= 1;
+     
+    while (dow>7)
+     dow -= 7;
+
+   result.dayOfWeek = dow;
+
+  return result;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+bool DS3231Time::isLeapYear(uint16_t year)
+{
+  return (year % 4 == 0);
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+uint16_t DS3231Time::date2days(uint16_t _year, uint8_t _month, uint8_t _day)
+{
+    _year = _year - 2000;
+
+    uint16_t days16 = _day;
+
+    for (uint8_t i = 1; i < _month; ++i)
+    {
+        days16 += pgm_read_byte(daysArray + i - 1);
+    }
+
+    if ((_month == 2) && isLeapYear(_year))
+    {
+        ++days16;
+    }
+
+    return days16 + 365 * _year + (_year + 3) / 4 - 1;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+long DS3231Time::time2long(uint16_t days, uint8_t hours, uint8_t minutes, uint8_t seconds)
+{    
+    return ((days * 24L + hours) * 60 + minutes) * 60 + seconds;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+DS3231Clock::DS3231Clock()
+{
+  wireInterface = getWireInterface(0);
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+TwoWire* DS3231Clock::getWireInterface(uint8_t wireNumber)
+{
+#if defined (__arm__) && defined (__SAM3X8E__)  
+    if(wireNumber == 1)
+      return &Wire1;
+    else
+      return &Wire;
+#else
+    return &Wire;
+#endif      
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+uint8_t DS3231Clock::dec2bcd(uint8_t val)
+{
+  return( (val/10*16) + (val%10) );
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+uint8_t DS3231Clock::bcd2dec(uint8_t val)
+{
+  return( (val/16*10) + (val%16) );
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+void DS3231Clock::setTime(const DS3231Time& time)
+{
+  setTime(time.second, time.minute, time.hour, time.dayOfWeek, time.dayOfMonth, time.month,time.year);
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+void DS3231Clock::setTime(uint8_t second, uint8_t minute, uint8_t hour, uint8_t dayOfWeek, uint8_t dayOfMonth, uint8_t month, uint16_t year)
+{
+
+  while(year > 100) // приводим к диапазону 0-99
+    year -= 100;
+ 
+  wireInterface->beginTransmission(DS3231Address);
+  
+  wireInterface->write(0); // указываем, что начинаем писать с регистра секунд
+  wireInterface->write(dec2bcd(second)); // пишем секунды
+  wireInterface->write(dec2bcd(minute)); // пишем минуты
+  wireInterface->write(dec2bcd(hour)); // пишем часы
+  wireInterface->write(dec2bcd(dayOfWeek)); // пишем день недели
+  wireInterface->write(dec2bcd(dayOfMonth)); // пишем дату
+  wireInterface->write(dec2bcd(month)); // пишем месяц
+  wireInterface->write(dec2bcd(year)); // пишем год
+  
+  wireInterface->endTransmission();
+
+  delay(10); // немного подождём для надёжности
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+DS3231Temperature DS3231Clock::getTemperature()
+{
+ DS3231Temperature res;
+  
+ union int16_byte {
+       int i;
+       byte b[2];
+   } rtcTemp;
+     
+  wireInterface->beginTransmission(DS3231Address);
+  wireInterface->write(0x11);
+  
+  if(wireInterface->endTransmission() != 0) // ошибка
+    return res;
+
+  if(wireInterface->requestFrom(DS3231Address, 2) == 2)
+  {
+    rtcTemp.b[1] = wireInterface->read();
+    rtcTemp.b[0] = wireInterface->read();
+
+    long tempC100 = (rtcTemp.i >> 6) * 25;
+
+    res.Value = tempC100/100;
+    res.Fract = abs(tempC100 % 100);
+    
+  }
+  
+  return res;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+DS3231Time DS3231Clock::getTime()
+{
+  DS3231Time t;
+
+  wireInterface->beginTransmission(DS3231Address);
+  wireInterface->write(0); // говорим, что мы собираемся читать с регистра 0
+  
+  if(wireInterface->endTransmission() != 0) // ошибка
+    return t;
+  
+  if(wireInterface->requestFrom(DS3231Address, 7) == 7) // читаем 7 байт, начиная с регистра 0
+  {
+      t.second = bcd2dec(wireInterface->read() & 0x7F);
+      t.minute = bcd2dec(wireInterface->read());
+      t.hour = bcd2dec(wireInterface->read() & 0x3F);
+      t.dayOfWeek = bcd2dec(wireInterface->read());
+      t.dayOfMonth = bcd2dec(wireInterface->read());
+      t.month = bcd2dec(wireInterface->read());
+      t.year = bcd2dec(wireInterface->read());     
+      t.year += 2000; // приводим время к нормальному формату
+  } // if
+  
+  return t;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+const char* DS3231Clock::getTimeStr(const DS3231Time& t)
+{
+  char* writePtr = workBuff;
+  
+  if(t.hour < 10)
+    *writePtr++ = '0';
+  else
+    *writePtr++ = (t.hour/10) + '0';
+
+  *writePtr++ = (t.hour % 10) + '0';
+
+ *writePtr++ = ':';
+
+ if(t.minute < 10)
+  *writePtr++ = '0';
+ else
+  *writePtr++ = (t.minute/10) + '0';
+
+ *writePtr++ = (t.minute % 10) + '0';
+
+ *writePtr++ = ':';
+
+ if(t.second < 10)
+  *writePtr++ = '0';
+ else
+  *writePtr++ = (t.second/10) + '0';
+
+ *writePtr++ = (t.second % 10) + '0';
+
+ *writePtr = 0;
+
+ return workBuff;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+const char* DS3231Clock::getDateStr(const DS3231Time& t)
+{
+  char* writePtr = workBuff;
+  if(t.dayOfMonth < 10)
+    *writePtr++ = '0';
+  else
+    *writePtr++ = (t.dayOfMonth/10) + '0';
+  *writePtr++ = (t.dayOfMonth % 10) + '0';
+
+  *writePtr++ = '.';
+
+  if(t.month < 10)
+    *writePtr++ = '0';
+  else
+    *writePtr++ = (t.month/10) + '0';
+  *writePtr++ = (t.month % 10) + '0';
+
+  *writePtr++ = '.';
+
+  *writePtr++ = (t.year/1000) + '0';
+  *writePtr++ = (t.year % 1000)/100 + '0';
+  *writePtr++ = (t.year % 100)/10 + '0';
+  *writePtr++ = (t.year % 10) + '0';  
+
+  *writePtr = 0;
+
+  return workBuff;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+void DS3231Clock::begin(uint8_t wireNumber)
+{
+  wireInterface = getWireInterface(wireNumber);
+  wireInterface->begin();  
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+
--- MAIN/MAIN.ino
+++ MAIN/MAIN.ino
@@ -1,14 +1,208 @@
+#include <MPU9250_RegisterMap.h>
+#include <SparkFunMPU9250-DMP.h>
+//--------------------------------------------------------------------------------------------------------------------------------------
+#include "DS3231.h" // подключаем часы реального времени
+#include "LoRa.h" // LoRa
+#include "Ublox.h" // Ublox
+//--------------------------------------------------------------------------------------------------------------------------------------
+MPU9250_DMP imu; // наш MPU9250
+//--------------------------------------------------------------------------------------------------------------------------------------
+#define GPS_BAUD 115200 // скорость работы с портом GPS
+#define GPS_SERIAL Serial1 // какой Serial используем для работы с GPS
+Ublox M6_Gps; // наш GPS-трекер
+
+// Altitude - Latitude - Longitude - N Satellites
+float gpsArray[] = {0, 0, 0, 0};
+//--------------------------------------------------------------------------------------------------------------------------------------
+void onLoraReceive(int packetSize)
+{
+  if(packetSize < 1)
+    return;
+
+  // тут разбор пришедшего пакета
+/*  
+  // read packet
+  for (int i = 0; i < packetSize; i++) 
+  {
+    Serial.print((char)LoRa.read());
+  }
+ */
+}
 //--------------------------------------------------------------------------------------------------------------------------------------
 void setup() 
 {
-  // put your setup code here, to run once:
 
+  Serial.begin(115200);
+  GPS_SERIAL.begin(GPS_BAUD);
+  
+  RTCClock.begin(0); // часы на первом интерфейсе I2C (поменять на 1, если надо на второй интерфейс I2C
+  LoRa.onReceive(onLoraReceive);
+  
+  // LoRa.setPins(csPin, resetPin, irqPin);// set CS, reset, IRQ pin
+  // LoRa.begin(433E6);  // initialize ratio at 433 MHz
+  LoRa.receive(); // переключаемся на приём
+
+  // Call imu.begin() to verify communication with and
+  // initialize the MPU-9250 to it's default values.
+  // Most functions return an error code - INV_SUCCESS (0)
+  // indicates the IMU was present and successfully set up
+  if (imu.begin() != INV_SUCCESS)
+  {
+    while (1)
+    {
+      Serial.println("Unable to communicate with MPU-9250");
+      Serial.println("Check connections, and try again.");
+      Serial.println();
+      delay(5000);
+    }
+  }
+
+  // Use setSensors to turn on or off MPU-9250 sensors.
+  // Any of the following defines can be combined:
+  // INV_XYZ_GYRO, INV_XYZ_ACCEL, INV_XYZ_COMPASS,
+  // INV_X_GYRO, INV_Y_GYRO, or INV_Z_GYRO
+  // Enable all sensors:
+  imu.setSensors(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS);
+
+  // Use setGyroFSR() and setAccelFSR() to configure the
+  // gyroscope and accelerometer full scale ranges.
+  // Gyro options are +/- 250, 500, 1000, or 2000 dps
+  imu.setGyroFSR(2000); // Set gyro to 2000 dps
+  // Accel options are +/- 2, 4, 8, or 16 g
+  imu.setAccelFSR(2); // Set accel to +/-2g
+  // Note: the MPU-9250's magnetometer FSR is set at 
+  // +/- 4912 uT (micro-tesla's)
+
+  // setLPF() can be used to set the digital low-pass filter
+  // of the accelerometer and gyroscope.
+  // Can be any of the following: 188, 98, 42, 20, 10, 5
+  // (values are in Hz).
+  imu.setLPF(5); // Set LPF corner frequency to 5Hz
+
+  // The sample rate of the accel/gyro can be set using
+  // setSampleRate. Acceptable values range from 4Hz to 1kHz
+  imu.setSampleRate(10); // Set sample rate to 10Hz
+
+  // Likewise, the compass (magnetometer) sample rate can be
+  // set using the setCompassSampleRate() function.
+  // This value can range between: 1-100Hz
+  imu.setCompassSampleRate(10); // Set mag rate to 10Hz
+
+  
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+void printIMUData(void)
+{  
+  // After calling update() the ax, ay, az, gx, gy, gz, mx,
+  // my, mz, time, and/or temerature class variables are all
+  // updated. Access them by placing the object. in front:
+
+  // Use the calcAccel, calcGyro, and calcMag functions to
+  // convert the raw sensor readings (signed 16-bit values)
+  // to their respective units.
+  float accelX = imu.calcAccel(imu.ax);
+  float accelY = imu.calcAccel(imu.ay);
+  float accelZ = imu.calcAccel(imu.az);
+  float gyroX = imu.calcGyro(imu.gx);
+  float gyroY = imu.calcGyro(imu.gy);
+  float gyroZ = imu.calcGyro(imu.gz);
+  float magX = imu.calcMag(imu.mx);
+  float magY = imu.calcMag(imu.my);
+  float magZ = imu.calcMag(imu.mz);
+  
+  Serial.println("Accel: " + String(accelX) + ", " +
+              String(accelY) + ", " + String(accelZ) + " g");
+  Serial.println("Gyro: " + String(gyroX) + ", " +
+              String(gyroY) + ", " + String(gyroZ) + " dps");
+  Serial.println("Mag: " + String(magX) + ", " +
+              String(magY) + ", " + String(magZ) + " uT");
+  Serial.println("Time: " + String(imu.time) + " ms");
+  Serial.println();
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+void handleGPS()
+{
+   // работаем с GPS 
+
+  if(!GPS_SERIAL.available())
+    return;
+
+  while(GPS_SERIAL.available())
+  {
+        char c = GPS_SERIAL.read();
+         if (M6_Gps.encode(c)) 
+         {
+            gpsArray[0] = M6_Gps.altitude;
+            gpsArray[1] = M6_Gps.latitude;
+            gpsArray[2] = M6_Gps.longitude; 
+            gpsArray[3] = M6_Gps.sats_in_use;
+
+           for(byte i = 0; i < sizeof(gpsArray)/sizeof(gpsArray[0]); i++) 
+            {
+              Serial.print(gpsArray[i], 6);
+              Serial.print(" ");
+            }
+            
+            Serial.println("");         
+        }
+  }
+ 
+   
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+void handleIMU()
+{
+  // работаем с акселерометром
+
+ // dataReady() checks to see if new accel/gyro data
+  // is available. It will return a boolean true or false
+  // (New magnetometer data cannot be checked, as the library
+  //  runs that sensor in single-conversion mode.)
+  if ( imu.dataReady() )
+  {
+    // Call update() to update the imu objects sensor data.
+    // You can specify which sensors to update by combining
+    // UPDATE_ACCEL, UPDATE_GYRO, UPDATE_COMPASS, and/or
+    // UPDATE_TEMPERATURE.
+    // (The update function defaults to accel, gyro, compass,
+    //  so you don't have to specify these values.)
+    imu.update(UPDATE_ACCEL | UPDATE_GYRO | UPDATE_COMPASS);
+    printIMUData();
+  }
+  
 }
 //--------------------------------------------------------------------------------------------------------------------------------------
 void loop() 
 {
-  // put your main code here, to run repeatedly:
 
+  /*
+  DS3231Time tm = RTCClock.getTime();
+  
+  tm.dayOfMonth
+  tm.month
+  tm.year
+
+  tm.hour
+  tm.minute
+  tm.second
+
+  String timeString = RTCClock.getTimeStr(tm);
+  String dateString = RTCClock.getDateStr(tm);
+  */
+
+  /*
+   // отсыл пакета в LoRa
+   LoRa.beginPacket();
+   LoRa.write(buffer, length);
+   LoRa.endPacket();
+   LoRa.receive();
+   */
+
+
+  handleIMU();
+  handleGPS();
+
+  
 }
 //--------------------------------------------------------------------------------------------------------------------------------------
 
