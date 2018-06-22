d7e253d130e9c9fe0fc2c8addb5bd499ba42fe63
promavto <promavto@ntmp.ru>
2018-06-22 20:26:13 +0300

    EGTS_Test_01

---
+++ EGTS_Test_01/EGTS_Test_01.ino
@@ -0,0 +1,26 @@
+/*
+    Name:       EGTS_Test_01.ino
+    Created:	22.06.2018 19:12:38
+    Author:     Александр
+*/
+
+#include "de.h"     // ST_ANSWER
+#include "egts.h"
+//#include <stdlib.h> /* malloc */
+
+
+
+
+
+
+void setup()
+{
+
+
+}
+
+void loop()
+{
+
+
+}
