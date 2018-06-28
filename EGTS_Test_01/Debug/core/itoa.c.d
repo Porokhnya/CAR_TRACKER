0dd1601b0a3c22718bf326b9dda395abeb15fa46
promavto <promavto@ntmp.ru>
2018-06-28 07:11:04 +0300

    Синхро

---
+++ EGTS_Test_01/Debug/core/itoa.c.d
@@ -0,0 +1,3 @@
+C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\EGTS_Test_01\arduino_due_x_dbg\Debug\core\itoa.c.o: \
+ C:\Users\moseichuk\AppData\Local\arduino15\packages\arduino\hardware\sam\1.6.11\cores\arduino\itoa.c \
+ C:\Users\moseichuk\AppData\Local\arduino15\packages\arduino\hardware\sam\1.6.11\cores\arduino\itoa.h
---
+++ EGTS_Test_01/Debug/egts.c.d
@@ -0,0 +1,5 @@
+C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\EGTS_Test_01\arduino_due_x_dbg\Debug\egts.c.o: \
+ C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\EGTS_Test_01\arduino_due_x_dbg\Debug\egts.c \
+ C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\EGTS_Test_01\arduino_due_x_dbg\Debug\de.h \
+ C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\EGTS_Test_01\arduino_due_x_dbg\Debug\lib.h \
+ C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\EGTS_Test_01\arduino_due_x_dbg\Debug\egts.h
